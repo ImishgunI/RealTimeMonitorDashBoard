65fdc627495bf42b9b1d1d35575c2e7f65ac6ae7
Michael <mvasilyev04@gmail.com>
2026-01-09 01:07:14 +0300

    create module utils and file, functions for finding temreture from diffirent files

---
+++ backend/utils/TempretureFilePath.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"os"
+	"strings"
+)
+
+func FindFilePathForCPUTemreture() string {
+	if isExist("/sys/class/thermal/thermal_zone0/temp") {
+		return "/sys/class/thermal/thermal_zone0/temp"
+	}
+	name := "/sys/class/hwmon/"
+	dirs, _ := os.ReadDir(name)
+	c := 0
+	names := []string{}
+	for _, entry := range dirs {
+		names = append(names, name+entry.Name()+"/")
+		for c < len(names) {
+			if isExist(names[c] + "name") {
+				dev, err := readFile(names[c] + "name")
+				if err != nil {
+					break
+				}
+				if dev == "k10temp" {
+					return names[c] + "temp1_input"
+				} else {
+					break
+				}
+			}
+		}
+		c++
+	}
+	return ""
+}
+
+func isExist(filepath string) bool {
+	_, err := os.Stat(filepath)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func readFile(filename string) (string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	str := strings.Trim(string(data), "\n")
+	return str, nil
+}
